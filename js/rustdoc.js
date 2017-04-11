b68414464e84802b266b352114be910678ccbd9d
Sean McArthur <sean.monstar@gmail.com>
2017-04-10 17:07:53 -0700

    fix rustdoc regex

--- js/rustdoc.js
+++ js/rustdoc.js
@@ -1,5 +1,5 @@
 document.querySelectorAll('pre > code').forEach(function (el) {
   var s = el.innerHTML.replace(/#\s.*\n/g, '');
-  s = s.replace(/<span class="error">#</span>\s.*\n/g, '');
+  s = s.replace(/<span class="error">#<\/span>\s.*\n/g, '');
   el.innerHTML = s;
 });

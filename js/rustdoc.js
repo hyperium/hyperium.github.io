4d793226026dd61aaf43eb60561b78cbe4897741
Sean McArthur <sean.monstar@gmail.com>
2017-04-10 17:01:55 -0700

    replace rouged rustdocs also

--- js/rustdoc.js
+++ js/rustdoc.js
@@ -1,3 +1,5 @@
 document.querySelectorAll('pre > code').forEach(function (el) {
-  el.innerHTML = el.innerHTML.replace(/#\s.*\n/g, '');
+  var s = el.innerHTML.replace(/#\s.*\n/g, '');
+  s = s.replace(/<span class="error">#</span>\s.*\n/g, '');
+  el.innerHTML = s;
 });

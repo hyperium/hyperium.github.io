d24c0272163cd2b8238c8a95a336fa392812fcd6
Sean McArthur <sean@seanmonstar.com>
2019-12-10 13:03:23 -0800

    Fix replacing hidden lines again

--- js/rustdoc.js
+++ js/rustdoc.js
@@ -1,5 +1,6 @@
 // All Rust lines starting with `# ` get the `.cs` (comment-special)
 // class. Remove them as they are only needed to make `rustdoc` happy.
-document.querySelectorAll('.language-rust pre > code .cs').forEach(function (el) {
-  el.parentElement.removeChild(el);
+document.querySelectorAll('.language-rust pre > code').forEach(function (el) {
+  var s = el.innerHTML.replace(/<span class="cs">#\s.*\n/g, '');
+  el.innerHTML = s;
 });
